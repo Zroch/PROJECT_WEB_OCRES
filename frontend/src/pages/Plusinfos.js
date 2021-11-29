fdd163d972a093d869f1954f2e00c741b7ffd653
Alexis YOUF <alexis.youf@edu.ece.fr>
2021-11-29 14:37:41 +0100

    info V2

--- frontend/src/pages/Plusinfos.js
+++ frontend/src/pages/Plusinfos.js
@@ -3,7 +3,11 @@ import React from 'react';
 import Aside from '../composants/Aside';
 import '../styles/Main.css';
 
-
+/***************************************************************************
+ * On retrouve sur cette page l'ensemble des informations indiquées sur la 
+ * page "plus d'info" du site web.
+ * Avec Frontend Backend et auteurs.
+ ***************************************************************************/
 function PlusInfo() {
     return (
 
@@ -17,31 +21,31 @@ function PlusInfo() {
           <div className="info">
           <h2>FrontEnd :</h2>
             Notre site web NAVTODAY à pour but de fournir aux utilisateurs des informations sur les conditions de navigation actuelle aux environs d'une ville.
-            Ses informations sont facilement disponible sur internet, mais aucun service ne les proposent sur une seule et même platforme.
+            Ces informations sont facilement disponible sur internet, mais aucun service ne les proposent sur une seule et même platforme.
             Pour changer de ville il suffit d'aller dans la section réglage.
             Vous pouvez alors retrouver :
-            <div className="liste">
+          <div className="liste">
             <li>La météo</li>
             <li>La visibilité</li>
             <li>Le vent</li>
-            <li>Les marées</li>
+            <li>la marée</li>
             <li>L'astronomie</li>
             <li>Les informations sur le port le plus proche</li>
-            </div>
-            <h2>BackEnd :</h2>
-            Par la suite les utilisateurs pourront alors rentrer les informations sur la capitainerie d'un port associé à une ville.
-            <div className="liste">
+          </div>
+          <h2>BackEnd :</h2>
+          Par la suite les utilisateurs pourront alors rentrer les informations sur la capitainerie d'un port associé à une ville.
+          <div className="liste">
             <li>Nom du port</li>
             <li>Nombre de places visiteurs</li>
             <li>Horaires</li>
             <li>Prix</li>
             <li>Présence de portes ou non</li>
-            </div>
+          </div>
 
-            <h3>Auteurs :</h3>
-            Lucas LESCOT & Alexis YOUF
-           </div>
-        </div>
+           <h3>Auteurs :</h3>
+          Lucas LESCOT & Alexis YOUF
+       </div>
+      </div>
       </div>
     </div>
     </div>
