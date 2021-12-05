a91557574c9bd61593feed9ec570db7a20a44685
Lucas Lescot <lucas.lescot@edu.ece.fr>
2021-12-05 13:56:33 +0100

    Info back end

---
+++ frontend/src/composants/Info6.js
@@ -0,0 +1,86 @@
+//Les imports
+import '../styles/Main.css';
+import React from "react";
+import { NavLink } from 'react-router-dom';
+
+
+//Ce compostant permet dafficher les boutons et l'information numéro 6 qui est le backend 
+
+class Info6 extends React.Component {
+//Les boutons sont affichés à l'aide de Navlink
+
+  render() {
+    return (
+      <div class="container">
+
+<div class="row">
+          <h1 className="font">Overview</h1>
+        </div>
+
+        <div class="row" >
+          <div class="col" className="top">
+            <button className="butts">
+              <NavLink to="/" className="navlink"
+                activeClassName="nav-active">
+                Méteo 
+              </NavLink>
+            </button >
+
+            <button className="butts">
+              <NavLink to="/visibilite" className="navlink"
+                activeClassName="nav-active">
+                Visiblité
+              </NavLink>
+            </button >
+
+            <button className="butts">
+              <NavLink to="/vent" className="navlink"
+                activeClassName="nav-active">
+                Vent
+              </NavLink>
+            </button >
+          </div>
+        </div>
+
+        <div class="row" >
+          <div class="col" className="top">
+          <button className="butts">
+              <NavLink to="/maree" className="navlink"
+                activeClassName="nav-active">
+                Marée
+              </NavLink>
+            </button >
+
+            <button className="butts">
+              <NavLink to="/astronomie" className="navlink"
+                activeClassName="nav-active">
+                Astronomie
+              </NavLink>
+            </button >
+
+            <button className="butts">
+              <NavLink to="/ports" className="navlink"
+                activeClassName="nav-active">
+                Ports
+              </NavLink>
+            </button >
+          </div>
+        </div>
+        
+        <div class="row">
+          <div className="mid">
+            <article>
+            
+            
+            
+            </article>
+          </div>
+        </div>
+      </div>
+      
+
+    );
+  }
+}
+
+export default Info6;
