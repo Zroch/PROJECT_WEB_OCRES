c25659e949c6f2ef3db7b7241b8417b5dff6e29e
Alexis YOUF <alexis.youf@edu.ece.fr>
2021-12-05 22:08:04 +0100

    Update

--- frontend/src/integrer/Form.js
+++ frontend/src/integrer/Form.js
@@ -34,9 +34,25 @@ class Form extends Component {
         axios.delete(`http://localhost:3000/api/ports/${id}`)
         .then(response => {
           window.alert(`le port ${id} a bien été supprimé`);
-        
+          
       });
     };
+
+    updateData = (id, city, name, adresse, horaire, price) => {
+
+      axios.put(`http://localhost:3000/api/ports/${id}`, {
+          id: id,
+          name: name,
+          city: city,
+          adresse: adresse,
+          horaire: horaire,
+          price: price
+      })
+      .then(response => {
+        window.alert(`le port ${id} a été mit à jour`);
+      
+    });
+  };
 
     render () {
         const { data } = this.state;
@@ -71,6 +87,30 @@ class Form extends Component {
                 <input type="text" onChange={e => this.setState({ id: e.target.value })} class="form-control" id="inputid"/>
 
                 <button className="btndelete" onClick={() => this.deleteData(this.state.id)}>Supprimer</button> 
+              </div>
+
+              <div className = "Update">
+                <br/>
+                <h2>Updater un port</h2>
+                <label for="id">ID</label>
+                <input type="text" onChange={e => this.setState({ id: e.target.value })} class="form-control" id="inputid"/>
+
+                <label for="inputcity">Ville</label>
+                  <input type="text" onChange={e => this.setState({ city: e.target.value })} class="form-control" id="inputcity"/>
+                       
+                  <label for="inputName">Nom du port</label>
+                  <input type="text"  onChange={e => this.setState({ name: e.target.value })} class="form-control" id="inputName"/>
+                       
+                  <label for="inputAdresse">Adresse</label>
+                  <input type="text" onChange={e => this.setState({ adresse: e.target.value })} class="form-control" id="inputAdresse"/>
+                      
+                  <label for="inputHoraires">Horaires</label>
+                  <input type="text" onChange={e => this.setState({ horaire: e.target.value })} class="form-control" id="inputHoraires"/>
+                       
+                  <label for="inputPrice">Prix</label>
+                  <input type="text" onChange={e => this.setState({ price: e.target.value })} class="form-control" id="inputPrice"/> 
+
+                <button className="btnupdate" onClick={() => this.updateData(this.state.id, this.state.city, this.state.name, this.state.adresse, this.state.horaire, this.state.price)}>Updater</button> 
               </div>
 
                  
